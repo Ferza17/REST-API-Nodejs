4366fab20fb98ef9dc04165c87c9ecaed1192445
Ferza17 <feryreza85@gmail.com>
2020-10-23 15:26:04 +0700

    adding image upload add fetch single post

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 /**
  * ========= Packages ==============
  */
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const http = require("http");
+const multer = require("multer");
 require("dotenv/config");
-
+// For Windows User
+const { uuidv4 } = require("uuid");
 /**
  * ========= End Packages ==============
  */
@@ -17,6 +20,26 @@ const app = express();
 /**
  * ========= Global Variable ==============
  */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4());
+  },
+});
+
+const filter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    return cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 /**
  * ========= Routes ==============
  */
@@ -28,7 +51,22 @@ const feedRoutes = require("./routes/feed");
  * ========= Initialize  ==============
  */
 app.use(bodyParser.json());
+app.use(
+  multer({
+    storage: storage,
+    fileFilter: filter,
+  }).single("image")
+);
 app.use("/feed", feedRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use((error, req, res, next) => {
+  console.log("error :>> ", error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({
+    message: message,
+  });
+});
 /**
  * ========= End Initialize ==============
  */
